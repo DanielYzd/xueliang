91b659b983adc42081beb3084868a4e992d17335
admin <admin@admindeMacBook-Pro.local>
2020-07-27 09:49:29 +0800

    重写提交

---
+++ src/components/OperationSummary/Recovery.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="height:100%;width:100%;">
+    <Echarts :isAxisChart="false" :chartData="chartData" style="height:100%;width:100%;"></Echarts>
+  </div>
+</template>
+
+<script>
+import Echarts from '../Common/Echarts'
+export default {
+  components: {
+    Echarts
+  },
+  data() {
+    return {
+      chartData: {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          // orient: 'vertical',
+          // left: 'left',
+          // data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
+            name: '本月故障恢复率',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              { value: 38, name: '故障自愈' },
+              { value: 24, name: '未恢复' },
+              { value: 28, name: '人工恢复' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-link to="/operation-summary">
+      <Button type="primary">运维汇总</Button>
+    </router-link>
   </div>
 </template>
 
 <script>
+import { Button } from 'element-ui'
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    Button
   }
-};
+}
 </script>
